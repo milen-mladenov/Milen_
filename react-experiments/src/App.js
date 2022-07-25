97c91361273589106ae0f6812645cd8b28d8ff4f
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-07-25 10:17:04 +0300

    starting to work on the dinamic generation of rows

---
+++ react-experiments/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {TableOrder} from './components/TableOrder/TableOrder'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <TableOrder />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-experiments/src/components/TableOrder/TableOrder.js
@@ -0,0 +1,45 @@
+import { TableRow } from "./TableRows/TableRow"
+import styles from './TableOrder.module.css'
+
+
+export const TableOrder = () => {
+    
+    const currentOrder = []
+
+    const items = {
+        name: "name",
+        note: "+",
+        price: "2.25",
+        count: "7",
+        currentOrder: 0
+    }
+    
+    return (
+        <table id="order_table" className={styles.order_table}>
+            <thead>
+            <tr id="order_table_head"  className={styles.order_table_head}>
+                <th>
+                    <p>коментар</p>
+                </th>
+                <th>
+                    <p>име артикул</p>
+                </th>
+                <th>
+                    <p>поръчани</p>
+                </th>
+                <th>
+                    <p>добави</p>
+                </th>
+                <th>
+                    <p>ед. сума</p>
+                </th>
+            </tr>
+            </thead>
+            
+            <tbody>
+            <TableRow items={items}/>
+
+            </tbody>
+        </table>
+    )
+}
